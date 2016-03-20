12cb1824135601c0e4f6d8f0957228de96c03b4b
antyufeev.vladimir <antyufeev.vladimir@gmail.com>
2016-03-20 15:10:45 +0200

    Fix bad response while log in

--- src/main/java/com/clicker/Clicker.java
+++ src/main/java/com/clicker/Clicker.java
@@ -175,12 +175,16 @@ public class Clicker {
             WebElement passElement = driver.findElement(By.name("password"));
             passElement.sendKeys(pass);
             passElement.submit();
-            driver.navigate().to(MAIN_URL + File.separator + PATH_URL);
+            String targetPath = MAIN_URL + File.separator + PATH_URL;
+            driver.navigate().to(targetPath);
+
+            //fix response error while login
+            if (!driver.getCurrentUrl().equals(targetPath)) {
+                driver.findElement(By.xpath("//*[@id=\"info\"]/div[3]/div/div[1]/a")).click();
+                driver.navigate().to(targetPath);
+            }
         } catch(Exception e) {
             throw new RegistrationException(e.getMessage());
         }
     }
-
-
-
 }
